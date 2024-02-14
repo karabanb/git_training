c503f98500578688e5cef00c4b2e3bc79f6a33d5
karabanb <bartlomiej.karaban@contractors.roche.com>
2024-02-14 12:20:40 +0100

    doctrinf added

--- some_script.py
+++ some_script.py
@@ -4,5 +4,13 @@
 
 
 def calculate_bmi(weight, height):
+   # write docstring
+
+    """
+    This function calculates the BMI of a person.
+    weight: weight of the person in kg
+    height: height of the person in meters
+    """   
+
     bmi = weight / (height ** 2)
     return bmi
