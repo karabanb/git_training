5ee7a8f0efd6c326505b8e10ec50ede6cbaa3c21
karabanb <bartlomiej.karaban@contractors.roche.com>
2024-02-14 12:26:45 +0100

    added next function

--- some_script.py
+++ some_script.py
@@ -2,7 +2,15 @@
 
 # write a fuction for calculating BMI. User should be able to input their weight and height.
 
+# smome comment
 
 def calculate_bmi(weight, height):
     bmi = weight / (height ** 2)
     return bmi
+
+
+def main():
+    weight = float(input("Enter your weight in kg: "))
+    height = float(input("Enter your height in meters: "))
+    bmi = calculate_bmi(weight, height)
+    print(f"Your BMI is {bmi}")
