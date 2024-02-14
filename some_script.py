94a26284924e4e225409f1edfb2f17262d638ddd
karabanb <bartlomiej.karaban@contractors.roche.com>
2024-02-14 12:13:49 +0100

    inital commit

---
+++ some_script.py
@@ -0,0 +1,8 @@
+
+
+# write a fuction for calculating BMI. User should be able to input their weight and height.
+
+
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
